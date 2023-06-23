5f2f378dcfc0212905c20a9b17889bb4e4e11cfb
Exe <alexjaramunoz@gmail.com>
2023-06-23 07:40:47 +0000

    Code of QuadTree

--- QuadTreeADT.h
+++ QuadTreeADT.h
@@ -1,46 +1,20 @@
 #pragma once
-#include <iostream>
 
-using namespace std;
+#include "Node.h"
 
-class Point{
-    private:
-        int x, y;
+class Quad {
+    Point topLeft;
+    Point botRight;
+    Node* n;
+    Quad* topLeftTree;
+    Quad* topRightTree;
+    Quad* botLeftTree;
+    Quad* botRightTree;
 
-    public:
-        Point(int a, int b){
-            x=a;
-            y=b;
-        }
-};
-
-class QuadTree{
-    private:    
-        int total_points, white_nodes, black_nodes;
-
-    public:
-        QuadTree(Point a, Point b){
-            total_points = 0;
-            white_nodes = 0;
-            black_nodes = 0;
-        }
-        
-        ~QuadTree(){}
-        
-        int totalPoints(){
-            return total_points;
-        }
-        
-        int totalNodes(){   //No estoy seguro si te piden el total de nodos o ambos por separado. Discutamoslo entre los tres!
-            //cout<<"White Nodes: "<<white_nodes<<" || Black Nodes: "<<black_nodes<<endl;
-            return white_nodes + black_nodes;
-        }
-        
-        void insert(Point p, int d){}
-
-        //list(){}
-        
-        int countRegion(Point p, int d){}
-    
-        int AggregateRegion(Point p, int d){}
+public:
+    Quad();
+    Quad(Point topL, Point botR);
+    void insert(Node*);
+    Node* search(Point);
+    bool inBoundary(Point);
 };
--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #pragma once
+#include "QuadTreeADT.h"
 #include <iostream>
+
 
 using namespace std;
 
